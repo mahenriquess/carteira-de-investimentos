f49a53251a092fc7f1dfe59909b64e9ccb328b7a
Marcelo Henrique da Silva <mahenriquess@outlook.com>
2019-12-09 18:46:18 -0300

    Adicionando simbolos por rota

--- carteira-investimentos-api/api/controllers/stockPrice.js
+++ carteira-investimentos-api/api/controllers/stockPrice.js
@@ -2,7 +2,7 @@ const stockPriceHelper = require('../helpers/stockPrice');
 
 module.exports = {
     get: async (app, req, res) => {
-        const data = await stockPriceHelper.getData("bidi4");
+        const data = await stockPriceHelper.getData(req.params.symbol);        
         res.json(data);
     }
 }
--- carteira-investimentos-api/api/routes/stockPrice.js
+++ carteira-investimentos-api/api/routes/stockPrice.js
@@ -2,7 +2,7 @@ const prefix = '/stock-price';
 
 
 module.exports = app => {
-    app.get(`${prefix}/`, (req, res) => {
+    app.get(`${prefix}/:symbol`, (req, res) => {
         app.api.controllers.stockPrice.get(app, req, res);
     });
 }
